e15e71c0073b6752361df80a6c46403fa23ebfca
LuccasgDev <163527503+LuccasgDev@users.noreply.github.com>
2025-01-19 06:35:37 -0300

    Delete C#/BankeysAPI/BankeysAPI directory
    correção

--- C#/BankeysAPI/BankeysAPI/Configurations/DatabaseConfig.cs
+++
@@ -1,5 +0,0 @@
-public static class DatabaseConfig
-{
-    public static string ConnectionString =>
-        "Host=bankeys.cry8kwikgjkh.us-east-1.rds.amazonaws.com;Port=5432;Database=bankeys;Username=postgres;Password=Edupaunocu4055";
-}
--- C#/BankeysAPI/BankeysAPI/Models/Transaction.cs
+++
@@ -1,7 +0,0 @@
-public class Transaction
-{
-    public int Id { get; set; }
-    public int AccountId { get; set; }
-    public decimal Amount { get; set; }
-    public DateTime Date { get; set; }
-}
